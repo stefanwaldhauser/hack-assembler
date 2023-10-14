2dd3b866e953f73eb18407a069e46977daa1e6af
Stefan Waldhauser <stefan.waldhauser@proton.me>
2023-10-14 14:48:19 +0200

    Added command line wrapper

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,19 @@
 #!/usr/bin/env node
+import fs from "fs";
+import { Command } from "commander";
+import { assemble } from "./assemble";
 
 async function run() {
-  console.log("Hello World");
+  const program = new Command()
+    .name("hack-assembler")
+    .version("0.1.0")
+    .requiredOption("-i, --inputFile <path>")
+    .requiredOption("-o, --outputFile <path>");
+  program.parse();
+  const { inputFile, outputFile } = program.opts();
+  const input = fs.createReadStream(inputFile);
+  const output = fs.createWriteStream(outputFile, { flags: "w" });
+  await assemble(input, output);
 }
 
 run();
