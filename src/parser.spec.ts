3e2c6feb22c79e9e0fd857a73d0fbf472b28ba16
Stefan Waldhauser <stefan.waldhauser@proton.me>
2023-10-14 13:20:27 +0200

    Added methods to parse comp, dest, jump part of c-instruction

---
+++ src/parser.spec.ts
@@ -0,0 +1,105 @@
+import assert from "assert";
+import {
+  instructionType,
+  INSTRUCTION_TYPE,
+  clean,
+  parseDest,
+  parseComp,
+  parseJump,
+} from "./parser";
+
+describe("clean", () => {
+  it("should remove comments", () => {
+    const input = "D=M // Set D register to M";
+    const result = clean(input);
+    assert.strictEqual(result, "D=M");
+  });
+
+  it("should trim leading and trailing whitespace", () => {
+    const input = "   A=D   ";
+    const result = clean(input);
+    assert.strictEqual(result, "A=D");
+  });
+
+  it("should throw an error for an empty instruction", () => {
+    const input = "";
+    assert.throws(() => clean(input), /Empty Instruction/);
+  });
+
+  it("should handle instructions without comments or extra whitespace", () => {
+    const input = "M=D";
+    const result = clean(input);
+    assert.strictEqual(result, "M=D");
+  });
+});
+
+describe("instructionType", () => {
+  it("should return A_INSTRUCTION for valid A-instructions", () => {
+    const result = instructionType("@R0");
+    assert.strictEqual(result, INSTRUCTION_TYPE.A_INSTRUCTION);
+  });
+
+  it("should return L_INSTRUCTION for valid L-instructions", () => {
+    const result = instructionType("(LOOP)");
+    assert.strictEqual(result, INSTRUCTION_TYPE.L_INSTRUCTION);
+  });
+
+  it("should return C_INSTRUCTION for valid C-instructions", () => {
+    const result = instructionType("D=M");
+    assert.strictEqual(result, INSTRUCTION_TYPE.C_INSTRUCTION);
+  });
+});
+
+describe("parseDest", () => {
+  it("should parse the destination mnemonic of a valid C-instruction", () => {
+    const input = "D=M+1;JMP";
+    const result = parseDest(input);
+    assert.strictEqual(result, "D");
+  });
+
+  it("should return null if dest is missing from the valid C-instruction", () => {
+    const input = "M+1;JMP";
+    const result = parseDest(input);
+    assert.equal(result, null);
+  });
+});
+
+describe("parseComp", () => {
+  it('should parse the computation mnemonic of a valid C-instruction with both "=" and ";"', () => {
+    const input = "D=M+1;JMP";
+    const result = parseComp(input);
+    assert.strictEqual(result, "M+1");
+  });
+
+  it('should parse the computation mnemonic of a valid C-instruction with "=" but no ";" (missing jump part)', () => {
+    const input = "D=M+1";
+    const result = parseComp(input);
+    assert.strictEqual(result, "M+1");
+  });
+
+  it('should parse the computation mnemonic of a valid C-instruction with ";" but no "=" (missing dest part)', () => {
+    const input = "M+1;JMP";
+    const result = parseComp(input);
+    assert.strictEqual(result, "M+1");
+  });
+
+  it('should return the whole instruction as CompMnemonics when no "=" or ";" is present (missing both dest and jump parts)', () => {
+    const input = "M+1";
+    const result = parseComp(input);
+    assert.strictEqual(result, "M+1");
+  });
+});
+
+describe("parseJump", () => {
+  it('should parse the jump mnemonic of a valid C-instruction with ";"', () => {
+    const input = "D=M+1;JMP";
+    const result = parseJump(input);
+    assert.strictEqual(result, "JMP");
+  });
+
+  it('should return null for a valid C-instruction without ";"', () => {
+    const input = "D=M+1";
+    const result = parseJump(input);
+    assert.strictEqual(result, null);
+  });
+});
---
+++ src/parser.ts
@@ -0,0 +1,83 @@
+import { CompMnemonics, DestMnemonics } from "./types";
+
+export enum INSTRUCTION_TYPE {
+  A_INSTRUCTION, // @xxx
+  C_INSTRUCTION, // dest=comp;jump
+  L_INSTRUCTION, // (label)
+}
+
+export const clean = (instruction: string) =>
+  [instruction]
+    .map((str) => {
+      const commentIndex = str.indexOf("//");
+      if (commentIndex !== -1) {
+        return str.substring(0, commentIndex);
+      } else {
+        return str;
+      }
+    })
+    .map((str) => str.trim())
+    .map((str) => {
+      if (str.length === 0) {
+        throw new Error("Empty Instruction");
+      }
+      return str;
+    })[0];
+
+export const instructionType = (instruction: string): INSTRUCTION_TYPE => {
+  const cleanedInstruction = clean(instruction);
+  if (cleanedInstruction.charAt(0) === "@") {
+    return INSTRUCTION_TYPE.A_INSTRUCTION;
+  } else if (
+    cleanedInstruction.charAt(0) === "(" &&
+    cleanedInstruction.charAt(cleanedInstruction.length - 1) === ")"
+  ) {
+    return INSTRUCTION_TYPE.L_INSTRUCTION;
+  } else {
+    return INSTRUCTION_TYPE.C_INSTRUCTION;
+  }
+};
+
+export const parseDest = (cInstruction: string): DestMnemonics | null => {
+  const cleanedInstruction = clean(cInstruction);
+  const equalsIndex = cleanedInstruction.indexOf("=");
+  const hasDestPart = equalsIndex !== -1;
+  if (!hasDestPart) {
+    return null;
+  }
+  return cleanedInstruction.substring(0, equalsIndex) as DestMnemonics;
+};
+
+export const parseJump = (cInstruction: string): DestMnemonics | null => {
+  const cleanedInstruction = clean(cInstruction);
+  const semiColonIndex = cleanedInstruction.indexOf(";");
+  const hasJumpPart = semiColonIndex !== -1;
+  if (!hasJumpPart) {
+    return null;
+  }
+  return cleanedInstruction.substring(
+    semiColonIndex + 1,
+    cleanedInstruction.length,
+  ) as DestMnemonics;
+};
+
+export const parseComp = (cInstruction: string): CompMnemonics => {
+  const cleanedInstruction = clean(cInstruction);
+  const equalsIndex = cleanedInstruction.indexOf("=");
+  const semiColonIndex = cleanedInstruction.indexOf(";");
+  const hasDestPart = equalsIndex !== -1;
+  const hasJumpPart = semiColonIndex !== -1;
+
+  if (!hasDestPart && !hasJumpPart) {
+    return cleanedInstruction as CompMnemonics;
+  } else if (!hasDestPart && hasJumpPart) {
+    return cleanedInstruction.substring(0, semiColonIndex) as CompMnemonics;
+  } else if (hasDestPart && !hasJumpPart) {
+    return cleanedInstruction.substring(equalsIndex + 1) as CompMnemonics;
+  } else {
+    return cleanedInstruction.substring(
+      equalsIndex + 1,
+      semiColonIndex,
+    ) as CompMnemonics;
+  }
+};
